baab10db2b9e0762d35cf63f7029995167479668
Sourabh Tolani <sourabhtolani02@gmail.com>
2020-09-24 05:04:15 +0530

    Marley Spoon Web Challenge - Some cleanup

---
+++ config/initializers/contentful_constant.rb
@@ -0,0 +1,3 @@
+CONTENTFUL_SPACE_ID = Rails.application.credentials.contentful[:space_id]
+CONTENTFUL_ENVIRONMENT = Rails.application.credentials.contentful[:environment]
+CONTENTFUL_ACCESS_TOKEN = Rails.application.credentials.contentful[:access_token]
